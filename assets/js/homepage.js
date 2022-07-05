57563dcfe718728ef634b1cd780506c72cb7d0e5
frl <103608522+farrelldianni@users.noreply.github.com>
2022-07-05 12:15:11 -0400

    end of lesson 1

---
+++ assets/js/homepage.js
@@ -0,0 +1,13 @@
+var getUserRepos = function() {
+    //format the github api url 
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make url request
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+getUserRepos();
